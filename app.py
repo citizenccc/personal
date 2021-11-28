71088f2e39eafe865bede75e1d8526aa28588441
citizenccc <v.pisarev69@gmail.com>
2021-11-28 23:19:20 +0600

    added several simple apps

--- app.py
+++ app.py
@@ -1,5 +1,24 @@
-print ("   /")
-print ("  /")
-print (" /")
-print ("/")
-dsada
+'''
+num1 = int(input("Enter the first number: "))
+action = input("Enter desired action: ")
+num2 = int(input("Enter the second number: "))
+
+if action == "+":
+    print(num1 + num2)
+elif action == "-":
+    print(num1 - num2)
+elif action == ":" or action == "/":
+    print(num1/num2)
+else:
+    print(num1*num2)
+
+x = 2
+print (x == 2)'''
+
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity: ")
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
